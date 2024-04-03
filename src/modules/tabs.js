cf1552033372aa218e1665003246030ba4f494f5
Alex <lesha.rudiko@gmail.com>
2024-04-03 17:40:24 +0300

    update

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -1,37 +1,61 @@
-import { createDefended } from "./createmanager"
+import {
+    createDefended
+} from "./createmanager"
 
 export const answerQuestions = () => {
-const linksQuestionsBlock = document.querySelector('.tabs_all_list')
-const tabLinks = document.querySelectorAll('.link_tabs_btn')
-const tabContents = document.querySelectorAll('.tab_info_cont')
+    const linksQuestionsBlock = document.querySelector('.tabs_all_list')
+    const tabLinks = document.querySelectorAll('.link_tabs_btn')
+    const tabContents = document.querySelectorAll('.tab_info_cont')
 
+    const renderWorker = (target) => {
+        const objRandomWorker = createDefended()
 
-const changeTabBlock = (e) => {
-    e.preventDefault()
+        if (target.children.length === 1) {
+            target.insertAdjacentHTML('beforeEnd', `
+        
+        <div class="img_tab_">
+        <img src="${objRandomWorker.pathImg}" alt="img" class="worker_tabs_img">
+        <p class="name_worker">${objRandomWorker.name}</p>
+        <p class="job">
+            ${objRandomWorker.jobTitle}
+        </p>
+        <div class="block_connection__worker">
+            <div class="conn_phone">
+                <p class="tit_conn">
+                    Как связаться:
+                </p>
+                <p class="tit_phone_conn">
+                    ${objRandomWorker.phone}
+                </p>
+            </div>
 
-        if(e.target.className === 'link_tabs_btn'){
+            <div class="social_conn">
+                <a href="https://api.whatsapp.com/send?phone=89267616799" class="link_wats"></a>
+            </div>
+        </div>
+    </div>
+        `)
+        }
+    }
+
+    const changeTabBlock = (e) => {
+        e.preventDefault()
+
+        if (e.target.className === 'link_tabs_btn') {
             tabLinks.forEach((link, index) => {
-                if(link === e.target){
-                    const img = tabContents[index].querySelector('.worker_tabs_img')
-                    const name = tabContents[index].querySelector('.name_worker')
-                    const titleJob = tabContents[index].querySelector('.job')
-                    const phone = tabContents[index].querySelector('.tit_phone_conn')
-                    const objRandomWorker = createDefended()
-                    
+                if (link === e.target) {
+                    renderWorker(tabContents[index])
+
                     link.classList.add('_active_link')
                     tabContents[index].classList.add("active_tb")
 
-                    img.src = objRandomWorker.pathImg
-                    name.textContent = objRandomWorker.name
-                    titleJob.textContent = objRandomWorker.jobTitle
-                    phone.textContent = objRandomWorker.phone
-                } else{
+                } else {
                     link.classList.remove('_active_link')
                     tabContents[index].classList.remove("active_tb")
                 }
             })
         }
-}
+    }
 
-linksQuestionsBlock.addEventListener('click', changeTabBlock)
+    linksQuestionsBlock.addEventListener('click', changeTabBlock)
 }
